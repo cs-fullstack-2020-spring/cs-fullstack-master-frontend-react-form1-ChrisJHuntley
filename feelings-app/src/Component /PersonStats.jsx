b1f1f0a32d158abd1877d2aadf1b18d38588a4bf
chris huntley <cjhuntley4@gmail.com>
2020-03-20 11:20:59 -0500

    close

--- feelings-app/src/Component /PersonStats.jsx
+++ feelings-app/src/Component /PersonStats.jsx
@@ -10,13 +10,25 @@ class PersonStats extends Component{
              
         }
     }
-     
+    handleChange = (event) => {
+         
+        let theName = event.target.ClassName;
+        let theValue = event.target.value;
+        if (theName === "Name") {
+            this.setState({ [theName]: theValue })
+        } else if (theName === "Age") {
+            this.setState({ [theName]: theValue })
+        } else if (theName === "Mood") {
+            this.setState({ [theName]: theValue })
+        }
+    }
     ClickedSubmit=(event)=>{
         event.preventDefault();
         console.log('Clicked')
         console.log(this.state)
         this.setState({submition: true })
     }
+
    render(){
        if(this.state.ClickedSubmit){
            return(
@@ -29,16 +41,25 @@ class PersonStats extends Component{
            
            <div>
                <form action="">
+                   <fieldset> 
                    <legend>User Form</legend>
+                   <div>
                    <label htmlFor="Name">Name:</label>
-                       <input className='Name' type="text"/>
+                       <input className='Name' type="text" id='Name' value={this.state.Name} onChange={this.handleChange}/>
                    <hr></hr>
+                   </div>
+                   <div> 
                    <label htmlFor="Age">Age:</label>
-                   <input className='Age' type="text"/>
+                   <input className='Age' type="text" id='Age' value={this.state.Age} onChange={this.handleChange}/>
                    <hr/>
+                   </div>
+                   <div>
                    <label htmlFor="mood">Mood</label>
-                   <input className='Mood' type="type"/>
+                   <input className='Mood' type="type" value={this.state.Mood} onChange={this.handleChange}/>
+                   </div>
                    <button onClick={this.ClickedSubmit}>Submit</button>
+                   </fieldset>
+                   
 
 
                </form>
